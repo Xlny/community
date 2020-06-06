77891fa9ddf22689b4c96d8f57affbbabe962c94
xln <964141162@qq.com>
2020-06-06 13:46:31 +0800

    emm

--- src/main/java/study/xln/community/controller/IndexController.java
+++ src/main/java/study/xln/community/controller/IndexController.java
@@ -7,15 +7,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 //@Controller自动扫描识别控制器，允许这个类接收前端请求
 @Controller
-public class HelloController {
+public class IndexController {
 
     //@GetMapping注释确保Http get请求到/hello, 然后被映射到hello()方法
-    @GetMapping("/hello")
+    @GetMapping("/")  //匹配路径
     //@RequestParam将查询字符串参数的值绑定name到hello()方法的name参数中
     //@RequestParam(快捷键Ctrl+P)
-    public String hello(@RequestParam(name = "name") String name, Model model){
-       model.addAttribute("name",name);
-       //自动去找hello这个模板，在templates里面
-       return "hello";
+    public String index(){
+//       model.addAttribute("name",name);
+       //自动去找index这个模板，在templates里面
+       return "index";
     }
 }
